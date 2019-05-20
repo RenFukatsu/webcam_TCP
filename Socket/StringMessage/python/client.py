2d1c89dc244f44c3b8b18eaa6bd6fc0032224338
RenFukatsu <fukatsuren88@gmail.com>
2019-05-20 21:25:02 +0900

    python tcp socket

--- Socket/StringMessage/python/client.py
+++ Socket/StringMessage/python/client.py
@@ -8,6 +8,4 @@ import socket
 
 soc = socket.create_connection(("127.0.0.1", 5007))
 soc.sendall(b'hello')
-data = soc.recv(1024)
-print(data)
 soc.close()
--- Socket/StringMessage/python/server.py
+++ Socket/StringMessage/python/server.py
@@ -11,12 +11,10 @@ soc.bind(('127.0.0.1', 5007))
 soc.listen(1)
 while True:
     conn, address = soc.accept()
-    while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        print('data :', data, 'address : ', address)
-        
-        conn.sendall(b'Recived:' + data)
+    data = conn.recv(1024)
+    if not data:
+        break
+    print('data :', data, 'address : ', address)
+    
     conn.close()
 soc.close()
