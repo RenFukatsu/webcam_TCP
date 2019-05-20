cc5bb9c93f6e492c30e62944e6f042f258f57ec7
RenFukatsu <fukatsuren88@gmail.com>
2019-05-20 20:45:27 +0900

    python tcp socket

--- Socket/StringMessage/python/client.py
+++ Socket/StringMessage/python/client.py
@@ -6,7 +6,7 @@ Socket TCP Client
 
 import socket
 
-soc = socket.create_connection(("127.0.0.1", 50007))
+soc = socket.create_connection(("127.0.0.1", 5007))
 soc.sendall(b'hello')
 data = soc.recv(1024)
 print(data)
--- Socket/StringMessage/python/server.py
+++ Socket/StringMessage/python/server.py
@@ -7,7 +7,7 @@ Socket TCP Server
 import socket
 
 soc = socket.socket()
-soc.bind(('127.0.0.1', 50007))
+soc.bind(('127.0.0.1', 5007))
 soc.listen(1)
 while True:
     conn, address = soc.accept()
